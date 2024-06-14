49bdada2823868b3037b816ff1c21ef1d0f3ee24
채승현 <boyism80@gmail.com>
2024-06-14 21:53:58 +0900

    CPP 컴파일 안되는 현상 수정

--- Factory/CPP/ValueDeserializeFactory.cs
+++ Factory/CPP/ValueDeserializeFactory.cs
@@ -9,107 +9,131 @@ namespace ExcelTableConverter.Factory.CPP
 
         }
 
-        private static string WithOptional(object obj, string type, bool nullable)
+        private static string WithOptional(object obj, string type, bool nullable, bool amp)
         {
-            if(nullable && type.EndsWith('&') == false)
-                return $"any_cast<{type}&>({obj})";
-            else
-                return $"any_cast<{type}>({obj})";
+            if (nullable || amp)
+            {
+                type = $"const {type}";
+
+                if (type.EndsWith('&') == false)
+                    type = $"{type}&";
+            }
+
+            return $"any_cast<{type}>({obj})";
         }
 
         protected override string ArrayType(object obj, string root, string e, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: true), false);
+            var amp = true;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), false, amp);
         }
 
         protected override string BooleanType(object obj, string root, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: false), nullable);
+            var amp = false;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         protected override string DateRangeType(object obj, string root, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: true), nullable);
+            var amp = true;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         protected override string DateTimeType(object obj, string root, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: true), nullable);
+            var amp = true;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         protected override string DictionaryType(object obj, string root, string k, string v, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: true), false);
+            var amp = true;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), false, amp);
         }
 
         protected override string DoubleType(object obj, string root, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: false), nullable);
+            var amp = false;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         protected override string DslType(object obj, string root, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: true), nullable);
+            var amp = true;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         protected override string EnumType(object obj, string root, string e, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: false), nullable);
+            var amp = false;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         protected override string FloatType(object obj, string root, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: false), nullable);
+            var amp = false;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         protected override string IntType(object obj, string root, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: false), nullable);
+            var amp = false;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         protected override string LongType(object obj, string root, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: false), nullable);
+            var amp = false;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         protected override string ByteType(object obj, string root, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: false), nullable);
+            var amp = false;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         protected override string SbyteType(object obj, string root, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: false), nullable);
+            var amp = false;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         protected override string ShortType(object obj, string root, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: false), nullable);
+            var amp = false;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         protected override string UshortType(object obj, string root, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: false), nullable);
+            var amp = false;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         protected override string UintType(object obj, string root, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: false), nullable);
+            var amp = false;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         protected override string UlongType(object obj, string root, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: false), nullable);
+            var amp = false;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         protected override string StringType(object obj, string root, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: true), false);
+            var amp = true;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), false, amp);
         }
 
         protected override string TimeSpanType(object obj, string root, bool nullable, DataFormatOption option)
         {
-            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: true), nullable);
+            var amp = true;
+            return WithOptional(obj, new TypeFactory(Context).Build(root, amp: amp), nullable, amp);
         }
 
         public string Build(string type, string value)

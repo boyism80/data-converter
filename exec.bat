faf7f7f594e930fdb83aa44d3e8a61e265748aba
채승현 <boyism80@gmail.com>
2024-09-11 00:04:35 +0900

    배치파일 수정

--- exec.bat
+++ exec.bat
@@ -1,21 +1,19 @@
 @ECHO OFF
 
-SET SOURCE=C:\Users\cshyeon\fb\resources\table
-SET DEST=C:\Users\cshyeon\fb
-
+SET SOURCE=D:\Users\CSHYEON\Data\git\game\c++\fb
 CALL dotnet publish -c Release -o "bin"
 if ERRORLEVEL 1 GOTO END
 
 PUSHD bin
-CALL ExcelTableConverter.exe --dir=%SOURCE% --lang=c++
+CALL ExcelTableConverter.exe --dir=%SOURCE%\resources\table --lang=c++
 POPD
 
 if ERRORLEVEL 1 GOTO END
 
-DEL /s /q "%DEST%\include\fb\game\model.h"
-RMDIR /s /q "%DEST%\game\json"
-XCOPY "bin\output\class\server\*.h" "%DEST%\include\fb\game\*.h"
-XCOPY "bin\output\json\\server\*.json" "%DEST%\game\json\*.json"
+DEL /s /q "%SOURCE%\include\fb\game\model.h"
+RMDIR /s /q "%SOURCE%\game\json"
+XCOPY "bin\output\class\server\*.h" "%SOURCE%\include\fb\game\*.h"
+XCOPY "bin\output\json\\server\*.json" "%SOURCE%\game\json\*.json"
 
 GOTO SKIP_PAUSE
 :END
